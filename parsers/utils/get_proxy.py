bb14cc9057418d5fc1e969cd3ae0e1ba28152c7f
FoxZippo <4elovekpro@gmail.com>
2021-07-01 13:58:57 +0700

    feature/get-proxy

--- parsers/utils/get_proxy.py
+++ parsers/utils/get_proxy.py
@@ -1,2 +1,18 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
 def get_proxy():
-    pass
+    url = "https://free-proxy-list.net/"
+    soup = bs(requests.get(url).content, "html.parser")
+    proxies = []
+    for row in soup.find("table", attrs={"id": "proxylisttable"}).find_all("tr")[1:]:
+        tds = row.find_all("td")
+        try:
+            ip = tds[0].text.strip()
+            port = tds[1].text.strip()
+            host = f"{ip}:{port}"
+            proxies.append(host)
+        except IndexError:
+            continue
+    return proxies[0]
